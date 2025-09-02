4c75ba7b31959051c718181a412413b4aa43b649
Camélia AMIN HANDOYO <camhdyo@gmail.com>
2025-09-02 11:13:09 +1100

    Update lire.php

--- views/articles/lire.php
+++ views/articles/lire.php
@@ -1,9 +1,10 @@
+
 <link rel="stylesheet" href="/css/article.css">
 
 <div class="article-container">
-    <h1><?= htmlspecialchars($articles['titre']) ?></h1>
+    <h1><?= htmlspecialchars($article['titre']) ?></h1>
     <div class="article-content">
-        <p><?= nl2br(htmlspecialchars($articles['contenu'])) ?></p>
+        <p><?= nl2br(htmlspecialchars($article['contenu'])) ?></p>
     </div>
     <a href="/" class="back-home">Retour à l'accueil</a>
 </div>
